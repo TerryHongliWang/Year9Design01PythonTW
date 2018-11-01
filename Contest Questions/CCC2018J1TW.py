3c69c36064ec0ad00cebd16067d5f4197e33991a
Terry Wang <terry.wang@Y22-Design-Terry-Wang.local>
2018-11-01 11:57:35 -0400

    A demo of a username input string

--- Contest Questions/CCC2018J1TW.py
+++ Contest Questions/CCC2018J1TW.py
@@ -1,1 +1,20 @@
-print("CCC2018J1TW")
+num1 = (input(""))
+num2 = (input(""))
+num3 = (input(""))
+num4 = (input(""))
+nope = 0
+
+if num1 == 8 or num1 == 9:
+	print("Ignore")
+	if num2 == num3:
+		print("Ignore")
+		if num4 == 8 or num4 == 9:
+			print("Ignore")
+		else:
+			print ("Answer")
+	else:
+		print("Answer")
+else:
+	print("Answer")
+
+#Doesn't Work
---
+++ DIStringDemo101.py
@@ -0,0 +1,7 @@
+fName = input("Input First Name:")
+lName = input("Input Last Name:")
+
+userName = fName[0] + lName
+
+
+print(userName)
